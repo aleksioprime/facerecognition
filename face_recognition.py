72f7fea15f2a9f70e99f400e256c52749a5d468d
Aleksei Semochkin <aleksioprime@gmail.com>
2023-04-20 01:27:30 +0300

    edit programm

--- face_recognition.py
+++ face_recognition.py
@@ -17,32 +17,32 @@ detector = FER()
 print("Форма входного слоя модели: {}x{}".format(model_width, model_height))
 emotions = {}
 timeline = []
-# while True:
-#     success, frame = video_capture.read()
-#     if not success:
-#         print("Ошибка подключения к камере!!!")
-#         break
-#     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#     faces = detector.detect_emotions(rgb_frame)
-#     interested = 0
-#     for face in faces:
-#         x, y, w, h = face['box']
-#         cv2.rectangle(frame, (x, y), (x + w, y + h), (150, 150, 150), thickness=2)
-#         max_emotion = max(face['emotions'], key=face['emotions'].get)
-#         cv2.putText(frame,
-#                     text=max_emotion,
-#                     org=(x, y - 2), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-#                     fontScale=0.6, color=(255, 255, 255),
-#                     thickness=1, lineType=cv2.LINE_AA)
-#         if max_emotion in ['neutral', 'happy']:
-#             interested += 1
-#     percent = round(interested / len(faces) * 100)
-#     df = df.append({'frame': time, 'peoples': len(faces), 'interested': interested, 'percent': percent},
-#                    ignore_index=True)
-#     time += 1
-#     cv2.imshow('Video', frame)
-#     if cv2.waitKey(1) == ord(' '):
-#         break
+while True:
+    success, frame = video_capture.read()
+    if not success:
+        print("Ошибка подключения к камере!!!")
+        break
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    faces = detector.detect_emotions(rgb_frame)
+    interested = 0
+    for face in faces:
+        x, y, w, h = face['box']
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (150, 150, 150), thickness=2)
+        max_emotion = max(face['emotions'], key=face['emotions'].get)
+        cv2.putText(frame,
+                    text=max_emotion,
+                    org=(x, y - 2), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=0.6, color=(255, 255, 255),
+                    thickness=1, lineType=cv2.LINE_AA)
+        if max_emotion in ['neutral', 'happy']:
+            interested += 1
+    percent = round(interested / len(faces) * 100)
+    df = df.append({'frame': time, 'peoples': len(faces), 'interested': interested, 'percent': percent},
+                   ignore_index=True)
+    time += 1
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) == ord(' '):
+        break
 
 
 df.to_excel("output.xlsx")
